2411061a39d64006e34ff41102c3bf4e5ebfac9a
mbalon <balonmateusz@gmail.com>
2020-08-02 15:02:12 +0200

    Add search middle point of segment

---
+++ geomfuntools.py
@@ -0,0 +1,7 @@
+class LineSegment:
+    def __init__(self, point1: tuple, point2: tuple):
+        self.point1 = point1
+        self.point2 = point2
+
+    def find_middle_point_of_segment(self) -> tuple:
+        pass
