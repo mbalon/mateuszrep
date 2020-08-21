1a3a49405f923a5325f7df776c3d04d8b1177c88
mbalon <balonmateusz@gmail.com>
2020-08-21 21:34:04 +0200

    Add some test

--- geomfuntools.py
+++ geomfuntools.py
@@ -4,6 +4,10 @@ class GeomTools:
         return (point1[0] + point2[0]) / 2, (point1[1] + point2[1]) / 2
 
     @staticmethod
-    def is_rectangle_seperate(rect1: tuple, rect2: tuple) -> bool:
-        return rect1[0][0] + rect1[1] <= rect2[0][0] or rect1[0][1] + rect1[2] <= rect2[0][1] \
-                or rect2[0][0] + rect2[1] <= rect1[0][0] or rect2[0][1] + rect2[2] <= rect2[0][1]
+    def is_rectangle_separate(rect1: tuple, rect2: tuple) -> bool:
+        """
+        Rectangle should be enter as tuple ((coordinates of left bottom corner), length of side parallel to
+        horizontal axis, length of side parallel to vertical axis
+        """
+        return rect1[0][0] + rect1[1] <= rect2[0][0] or rect1[0][1] + rect1[2] <= rect2[0][1] or \
+               rect2[0][0] + rect2[1] <= rect1[0][0] or rect2[0][1] + rect2[2] <= rect2[0][1]
--- test/test_geomfuntools.py
+++ test/test_geomfuntools.py
@@ -18,19 +18,51 @@ def test_find_middle_point_of_segment_2():
 
     assert output == (0, 2)
 
-def test_is_rectangle_seperate_1():
+def test_find_middle_point_of_segment_3():
+    x = (7, 0)
+    y = (-1, 0)
+
+    output = GeomTools.find_middle_point_of_segment(x, y)
+
+    assert output == (3, 0)
+
+def test_find_middle_point_of_segment_4():
+    x = (0, 4)
+    y = (0, 0)
+
+    output = GeomTools.find_middle_point_of_segment(x, y)
+
+    assert output == (0, 2)
+
+def test_is_rectangle_separate_1():
     x = ((0, 0), 3, 3)
     y = ((4, 0), 4, 4)
 
-    output = GeomTools.is_rectangle_seperate(x, y)
+    output = GeomTools.is_rectangle_separate(x, y)
 
     assert output == True
 
-def test_is_rectangle_seperate_2():
+def test_is_rectangle_separate_2():
     x = ((0, 0), 3, 3)
     y = ((0, 0), 4, 4)
 
-    output = GeomTools.is_rectangle_seperate(x, y)
+    output = GeomTools.is_rectangle_separate(x, y)
+
+    assert output == False
+
+def test_is_rectangle_separate_3():
+    x = ((1, 1), 2, 2)
+    y = ((0, 0), 4, 4)
+
+    output = GeomTools.is_rectangle_separate(x, y)
+
+    assert output == False
+
+def test_is_rectangle_separate_4():
+    x = ((0, 0), 4, 4)
+    y = ((1, 1), 3, 3)
+
+    output = GeomTools.is_rectangle_separate(x, y)
 
     assert output == False
 
