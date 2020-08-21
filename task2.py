683fb4d2febf444f5e9d0e23c287d88555203e36
Kamil D <kamil.dr91@gmail.com>
2020-08-22 00:19:37 +0200

    Dodane opisy funkcji

--- task2.py
+++ task2.py
@@ -22,10 +22,12 @@ class MathEquatations:
         return a
 
     @staticmethod
+    # computes the square root of the sum of a sequence of numbers given as an argument, divided by the product of the sequence
     def operation_with_arithmetic_sequence(sequence:list) -> float:
         return math.sqrt(sum(sequence)/MathEquatations.count_product_of_list(sequence))
 
     @staticmethod
+    # determines the median of numbers, multiplied by half of PI and finally increased by the Epsilon constant
     def equatations_with_median(sequence:list) -> float:
         return statistics.median(sequence)*(math.pi/2) + sys.float_info.epsilon
 
